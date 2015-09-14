da0c679cf6967b4c0d3ddb1ed5adece8c4b2bc6f
lds00129 <lds00129@babelsoft.co.kr>
2015-09-14 18:31:39 +0900

    메소드에 대해서

---
+++ src/org/lds00129/javasin/eclipse/MethodDemo1.java
@@ -0,0 +1,15 @@
+package org.lds00129.javasin.eclipse;
+ 
+public class MethodDemo1 {
+    public static void numbering() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+ 
+    public static void main(String[] args) {
+        numbering();
+    }
+}
